6cd095f6728cd7da1d2de87830b6592cad66ba02
Aryan sharma <aryansharma99107@gmail.com>
2024-10-08 22:12:50 +0530

    insert

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust these paths to match your project structure
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
